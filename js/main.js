eb86fb96943cb7c1f771a502e2e2ad322012f935
susanxiao22 <xiasketchix@gmail.com>
2026-02-18 23:24:07 -0600

    stuff

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 // JS scripts placed here
+const toggleBtn = document.querySelector('#toggle-btn');
+const extraContent = document.querySelector('#extra-content');
+
+toggleBtn.addEventListener('click', function() {
+    
+    extraContent.classList.toggle('show-content');
+
+    if (extraContent.classList.contains('show-content')) {
+        toggleBtn.textContent = 'Hide Brewing Tip';
+    } else {
+        toggleBtn.textContent = 'Show Brewing Tip';
+    }
+});
